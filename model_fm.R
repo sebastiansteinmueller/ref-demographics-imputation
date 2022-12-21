9ea2abe84454012631687092b5d8be6d99a29076
Sebastian Steinmueller <sebastian.steinmueller@gmail.com>
2022-12-21 11:14:24 +0100

    remove regional effect, more chains, narrower prior on origin group sd

--- model_fm.R
+++ model_fm.R
@@ -21,11 +21,16 @@ load("data/dem_refvda_end2021.RData")
 
 ##### II. Define model formula #####
 
-f.m.fm <- bf(formula =  female | trials(total) ~ (1|origin_iso3/asylum_region/asylum_iso3) + # varying intercept
-                                        logGniRatio + logDistance + neighbor + # population level covariates
-                                        (0+logGniRatio + logDistance + neighbor|origin_iso3), # varying slopes by origin
+# f.m.fm <- bf(formula =  female | trials(total) ~ (1|origin_iso3/asylum_region/asylum_iso3) + # varying intercept
+#                                         logGniRatio + logDistance + neighbor + # population level covariates
+#                                         (0+logGniRatio + logDistance + neighbor|origin_iso3), # varying slopes by origin
+#              center = T # intercept centered at mean of population level covariates
+#             )
+f.m.fm <- bf(formula =  female | trials(total) ~ (1|origin_iso3/asylum_iso3) + # varying intercept
+               logGniRatio + logDistance + neighbor + # population level covariates
+               (0+logGniRatio + logDistance + neighbor|origin_iso3), # varying slopes by origin
              center = T # intercept centered at mean of population level covariates
-            )
+)
 
 
 ##### III. Define prior distributions ##### 
@@ -35,13 +40,19 @@ priors.m.fm.empty <- get_prior(f.m.fm,
           data = dem_longMissing %>% 
           filter(missing %in% c("none", "age")))
 
+# priors.m.fm <- c(
+#   prior(normal(0,10), class = Intercept),
+#   prior(student_t(7,0,2.5), class = b), # population-level parameters have a multiplicative effect on odds of female. Absolute values above ~3 (ten-fold reduction/increase in odds for doubling in distance/gni ratio) are implausible, thus choice of narrower prior with df=7
+#   prior(student_t(7,0,2.5), class = sd, group = "origin_iso3:asylum_region:asylum_iso3"),
+#   prior(student_t(7,0,2.5), class = sd, group = "origin_iso3:asylum_region"), # more pooling on region and country of asylum
+#   prior(student_t(5,0,2.5), class = sd, group = "origin_iso3") # less pooling, i.e. closer to separate model for each origin
+#   )
 priors.m.fm <- c(
   prior(normal(0,10), class = Intercept),
   prior(student_t(7,0,2.5), class = b), # population-level parameters have a multiplicative effect on odds of female. Absolute values above ~3 (ten-fold reduction/increase in odds for doubling in distance/gni ratio) are implausible, thus choice of narrower prior with df=7
-  prior(student_t(7,0,2.5), class = sd, group = "origin_iso3:asylum_region:asylum_iso3"),
-  prior(student_t(7,0,2.5), class = sd, group = "origin_iso3:asylum_region"), # more pooling on region and country of asylum
+  prior(student_t(7,0,2.5), class = sd, group = "origin_iso3:asylum_iso3"),
   prior(student_t(5,0,2.5), class = sd, group = "origin_iso3") # less pooling, i.e. closer to separate model for each origin
-  )
+)
 
 
 m.fm <- brm(formula = f.m.fm,
@@ -50,9 +61,9 @@ m.fm <- brm(formula = f.m.fm,
             prior = priors.m.fm,
             init = 0,
             sample_prior = "yes",
-            iter = 4000,
+            iter = 3000,
             cores = 4,
-            control=list(adapt_delta=0.95, 
+            control=list(adapt_delta=0.9, 
                           max_treedepth=12),
             seed = 938
             )
