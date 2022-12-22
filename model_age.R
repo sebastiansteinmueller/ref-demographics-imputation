84737384f0f5dde0872173f3115165f5492c7e24
Sebastian Steinmueller <sebastian.steinmueller@gmail.com>
2022-12-22 15:06:29 +0100

    define wpp priors for age categories

--- model_age.R
+++ model_age.R
@@ -19,26 +19,52 @@ library(piggyback)
 ### read dataset
 load("data/dem_refvda_end2021.RData")
 
+dem_longMissing$femaleAge  <- with(dem_longMissing, cbind(female_0_4, female_5_11, female_12_17, female_18_59, female_60))
+dem_longMissing$maleAge  <- with(dem_longMissing, cbind(male_0_4, male_5_11, male_12_17, male_18_59, male_60))
 
-##### II. Define model formula #####
 
-f.m.fm <- bf(formula =  female | trials(total) ~ (1|origin_iso3/asylum_iso3) + # varying intercept
+##### II. Define model formulas #####
+
+f.m.femaleAge <- bf(formula =  femaleAge | trials(female) ~ (1|origin_iso3/asylum_iso3) + # varying intercept
                logGniRatio + logDistance + neighbor + # population level covariates
                (0+logGniRatio + logDistance + neighbor|origin_iso3), # varying slopes by origin
+               family = multinomial(link = "logit", refcat = "female_18_59"),
              center = T # intercept centered at mean of population level covariates
 )
 
+f.m.maleAge <- bf(formula =  maleAge | trials(male) ~ (1|origin_iso3/asylum_iso3) + # varying intercept
+                      logGniRatio + logDistance + neighbor + # population level covariates
+                      (0+logGniRatio + logDistance + neighbor|origin_iso3), # varying slopes by origin
+                  family = multinomial(link = "logit", refcat = "male_18_59"),
+                    center = T # intercept centered at mean of population level covariates
+)
+
+f.m.age <- f.m.femaleAge + f.m.maleAge + set_rescor(FALSE)
 
 ##### III. Define prior distributions ##### 
 
-priors.m.fm.empty <- get_prior(f.m.fm,
-          family = binomial(link = "logit"),
+priors.m.age.empty <- get_prior(f.m.age,
           data = dem_longMissing %>% 
-          filter(missing %in% c("none", "age")))
+          filter(missing %in% c("none")))
 
 
-priors.m.fm <- c(
-  prior(normal(0,10), class = Intercept),
+# WPP 2021 world frequencies for centres of population-level age group intercepts (0-4, 5-11, 12-17, 18-59, 60+ sex-specific distributions)
+wpp_female <- c(0.082948798,
+                0.117108803,
+                0.094642446,
+                0.555118782, # 18-59, reference cat
+                0.15018117)
+wpp_male <- c(0.086824115,
+              0.123420862,
+              0.09993278,
+              0.56594869, # 18-59, reference cat
+              0.123873553)
+
+priors.m.age <- c(
+  prior(normal(log(wpp_female[1]/wpp_female[4]),1), class = Intercept, dpar = "mufemale04"),
+  prior(normal(log(wpp_female[2]/wpp_female[4]),1), class = Intercept, dpar = "mufemale511"),
+  prior(normal(log(wpp_female[3]/wpp_female[4]),1), class = Intercept, dpar = "mufemale1217"),
+  prior(normal(log(wpp_female[5]/wpp_female[4]),1), class = Intercept, dpar = "mufemale60"),
   prior(student_t(7,0,2.5), class = b), # population-level parameters have a multiplicative effect on odds of female. Absolute values above ~3 (ten-fold reduction/increase in odds for doubling in distance/gni ratio) are implausible, thus choice of narrower prior with df=7
   prior(student_t(7,0,2.5), class = sd, group = "origin_iso3:asylum_iso3"),
   prior(student_t(5,0,2.5), class = sd, group = "origin_iso3") # less pooling, i.e. closer to separate model for each origin
